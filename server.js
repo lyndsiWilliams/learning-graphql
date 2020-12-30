59573b243e261703e400b9f0c4da2e4a1bc836e2
lyndsiWilliams <kcatgirl@gmail.com>
2020-12-30 13:54:57 -0600

    Books field added to AuthorType

--- server.js
+++ server.js
@@ -52,7 +52,13 @@ const AuthorType = new GraphQLObjectType({
   description: 'This represents an author of a book',
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLNonNull(GraphQLString) }
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter(book => book.authorId == author.id)
+      }
+    }
   })
 })
 
